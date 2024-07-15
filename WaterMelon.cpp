e1ecc2d32762b46ab30a857b77bcd5805f04e058
SoyedFajol <157878922+SoyedFajol@users.noreply.github.com>
2024-07-15 23:13:47 +0600

    Add files via upload

---
+++ WaterMelon.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int w;
+    cin>>w;
+    if(w%2==1){
+        cout<<"No"<<endl;
+
+    }
+    else{
+        if(w==2)
+            cout<<"No"<<endl;
+        else cout<<"Yes"<<endl;
+    }
+
+
+
+}

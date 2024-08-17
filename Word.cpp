8f661acfd6eaad6a76f6de0a15ac5ffff5ac4149
SoyedFajol <157878922+SoyedFajol@users.noreply.github.com>
2024-08-17 23:41:36 +0600

    Add files via upload

---
+++ Word.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int u=0,l=0;
+    string s;
+    cin>>s;
+    for(int i=0; i<s.size(); i++)
+    {
+        if(isupper(s[i]))
+        {
+            u++;
+        }
+        else
+        {
+            l++;
+        }
+    }
+    if(u<l)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+
+    }
+      if(u>l)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=toupper(s[i]);
+        }
+        cout<<s<<endl;
+
+    }
+      if(u==l)
+    {
+        for(int i=0; i<s.size(); i++)
+        {
+            s[i]=tolower(s[i]);
+        }
+        cout<<s<<endl;
+
+    }
+}

fbed2f689378a23839bffdf8f50119ff372d3b56
SoyedFajol <157878922+SoyedFajol@users.noreply.github.com>
2024-09-16 23:26:55 +0600

    Add files via upload

---
+++ YES or YES.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+      int t;
+      cin>>t;
+      while(t--){
+        string s;
+        cin>>s;
+        int A=0;
+        if(s[0]=='Y' || s[0]=='y'){
+            A++;
+        }
+        if(s[1]=='e' || s[1]=='E'){
+            A++;
+        }
+        if(s[2]=='S' || s[2]=='s'){
+            A++;
+        }
+        if(A==3){
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+      }
+
+
+
+}

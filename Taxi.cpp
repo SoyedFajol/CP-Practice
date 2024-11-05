8df760fcef49ca6e281a5ae672807ba22dc66dcf
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2024-11-05 20:33:46 +0600

    Add files via upload

---
+++ Taxi.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    int a=0,b=0,c=0,d=0,min_t=0;
+    cin>>t;
+    int A[t];
+    for(int i=0; i<t; i++)
+    {
+        cin>>A[i];
+        if(A[i]==1)
+        {
+            a++;
+        }
+        if(A[i]==2)
+        {
+            b++;
+        }
+        if(A[i]==3)
+        {
+            c++;
+        }
+        if(A[i]==4)
+        {
+            d++;
+        }
+    }
+
+        min_t=d;
+        while(a!=0 && c!=0)
+        {
+            c=c-1;
+            a=a-1;
+            min_t=min_t+1;
+        }
+        if(a==0 && c!=0)
+        {
+            min_t=min_t+c;
+            c=0;
+        }
+        min_t=min_t+b/2;
+        if(b%2!=0)
+        {
+            if(a<=2)
+            {
+                min_t=min_t+1;
+                b=0;
+                c=0;
+            }
+            else
+            {
+                a=a+2;
+                min_t=min_t+1;
+                b=0;
+            }
+            if(a!=0)
+            {
+                min_t=min_t+a/4;
+                if(a%4!=0)
+                {
+                    min_t=min_t+1;
+                }
+            }
+        }
+        cout<<min_t;
+    }
+
+
+
+

828511e354a4ba8e0981b599e87d5d3ad75769b4
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2024-11-14 23:58:32 +0600

    Add files via upload

---
+++ Line Trip.cpp
@@ -0,0 +1,27 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,x;
+        cin>>n>>x;
+        vector<int> arr(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        int last=0;
+        int ans= INT_MIN;
+        for(int i=0; i<n; i++)
+        {
+            ans=max(ans,arr[i]-last);
+
+        }
+        ans= max(ans, 2*x-last);
+        cout<<ans<<endl;
+    }
+}

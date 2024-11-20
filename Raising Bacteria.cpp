2a5986825f0c64d5f1c6b87892f12834833e5048
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2024-11-21 00:16:53 +0600

    Add files via upload

--- Raising Bacteria.cpp
+++ Raising Bacteria.cpp
@@ -1,1 +1,15 @@
-
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long x;
+    cin>>x;
+    long long sum=0;
+    while(x!=0)
+    {
+        if(x%2==1) sum++;
+        x =x/2;
+    }
+    cout <<sum<<endl;
+    return 0;
+}

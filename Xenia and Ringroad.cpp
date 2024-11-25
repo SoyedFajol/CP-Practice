0f7a3e4ad05c1f00f4f304f054acc272fb7e1bea
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2024-11-25 22:21:30 +0600

    Add files via upload

--- Xenia and Ringroad.cpp
+++ Xenia and Ringroad.cpp
@@ -3,7 +3,25 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin>>t;
+    int a,b;
+    cin>>a>>b;
+    int arr[b];
+    for(int i=0; i<b; i++)
+    {
+        cin>>arr[i];
+    }
+       long long int count=0;
+       int current = 1;
 
+   for(int i=0;i<b;i++)
+   {
+       if(arr[i] >= current)
+           count += arr[i] - current;
+       else
+           count += a - current + arr[i];
+
+       current = arr[i];
+   }
+    cout<<count;
+    return 0;
 }

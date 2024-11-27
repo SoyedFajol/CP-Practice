ce440eced5cbcf17ef91ed8f6c4f7dfd937d7680
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2024-11-27 23:46:09 +0600

    Add files via upload

---
+++ Sereja and Dima.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    int sereja=0,dima=0;
+    int l=0;
+    int r=n-1;
+    int z=0;
+    while(l<=r)
+    {
+        if(z%2==0)
+        {
+            if(arr[l]>arr[r])
+            {
+                sereja+=arr[l];
+                l++;
+            }
+            else
+            {
+                sereja+=arr[r];
+                r--;
+            }
+
+
+        }
+        else
+        {
+             if(arr[l]>arr[r])
+            {
+                dima+=arr[l];
+                l++;
+            }
+            else
+            {
+                dima+=arr[r];
+                r--;
+            }
+    }
+    z++;
+}
+    cout<<sereja<<" "<<dima<<endl;
+    return 0;
+}

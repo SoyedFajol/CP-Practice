9237dde17db2196ffbfd265390f364f1b462589c
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2025-01-30 21:53:40 +0600

    Add files via upload

---
+++ Minimal Square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        if(a<b) a=2*a;
+        else b=2*b;
+        int area=max(a,b)*max(a,b);
+        cout<<area<<endl;
+    }
+    return 0;
+}

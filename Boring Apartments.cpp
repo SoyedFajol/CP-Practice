f5bfca3ca55f10913ad2b5baf9e588c64d321aba
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2025-02-18 22:41:17 +0600

    Add files via upload

---
+++ Boring Apartments.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a;
+        cin>>a;
+
+    int A=a;
+    int digit=a%10;
+    int count=0;
+    while(a>0)
+    {
+        a/=10;
+        count++;
+    }
+    if(count==1)
+    {
+        cout<<((digit*10)-10)+1<<endl;
+    }
+    else if(count==2)
+    {
+        cout<<((digit*10)-10)+3<<endl;
+    }
+    else if(count==3)
+    {
+        cout<<((digit*10)-10)+6<<endl;
+    }
+    else if(count==4)
+    {
+        cout<<((digit*10)-10)+10<<endl;
+    }
+}
+}

b6e1383ea95b38b55713c489559a4927651ec047
Relbai <157878922+SoyedFajol@users.noreply.github.com>
2025-03-06 23:44:41 +0600

    Add files via upload

---
+++ Vasya and Socks.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    for(int i=1; b*i<=a; i++)
+    {
+        a++;
+    }
+    cout<<a;
+
+
+}
